48c470e2bdd1d7d000cb8b755dc800bbcbefde57
mayankmokhere <mayankmokhere11@gmail.com>
2024-05-09 12:49:38 +0530

    yoo

---
+++ Components/Home/Header.js
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import React from 'react'
+import logo from '@/public/Img/logo.jpg'
+import { PiSignInBold } from "react-icons/pi";
+import { MdCreateNewFolder } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { FcAbout } from "react-icons/fc";
+import Link from 'next/link';
+function Header() {
+  return (
+    <>
+    <div className='flex justify-between p-2 border-b-[4px] border-red-600 bg-white'>
+        <div className='flex justify-between items-center gap-4'>
+            <Image className='h-20 w-20 rounded-full 'src={logo}/>
+        <div className='flex gap-5 ms-12 items-center'>
+            <Link href='/'><span className='hidden sm:block'>HOME</span><FaHome className='sm:hidden'/></Link>
+            <Link href='/About'><span className='hidden sm:block'>ABOUT</span><FcAbout  className='sm:hidden'/></Link>
+        </div>
+            
+        </div>
+        <div className='flex justify-around gap-4 items-center'>
+            {/* <button className='border-black border-[2px] rounded-full p-2 bg-black text-white'><span className='hidden sm:block'>CREATE POST</span><MdCreateNewFolder className='sm:hidden'/></button> */}
+            <button className='border-black border-[2px] rounded-xl p-2 bg-white'><span className='hidden sm:block'>SIGN IN</span><PiSignInBold className='sm:hidden'/></button>
+            <Image className='h-10 w-10 rounded-full 'src={logo}/>
+        </div>
+    </div>
+    </>
+
+  )
+}
+
+export default Header
---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "@/Components/Home/Header";
+export default function Home() {
+  return (
+    <main>
+      
+      <div>
+        {/* <Header/> */}
+      {/* <h1 className="text-3xl font-bold underline">Hello World</h1> */}
+    </div>
+    </main>
+  );
+}
